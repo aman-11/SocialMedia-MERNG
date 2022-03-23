6c506561f16fc73d664694ed461b0e8f043f0171
Aayush <aayushaman11@gmail..com>
2022-03-23 10:47:21 +0530

    changes in apollo server setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,28 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-//import ApolloProvider from "./ApolloServer";
 
-/*<App />  instead of render App now render the ApolloProvider as in this App is wrapped  http://localhost:3000 */
+//setting up the Apollo client
+import {
+  ApolloClient,
+  InMemoryCache, //caching
+  ApolloProvider,
+} from "@apollo/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//creating the client instance
+const client = new ApolloClient({
+  //   link: httpLink,
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
