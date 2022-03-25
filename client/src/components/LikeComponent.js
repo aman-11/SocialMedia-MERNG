e15fd52b2f49327e2fb3f96b8cec28a7c75b65ec
Aayush <aayushaman11@gmail..com>
2022-03-25 14:01:12 +0530

    added like functionality

---
+++ client/src/components/LikeComponent.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { Button, Icon, Label } from "semantic-ui-react";
+import { gql, useMutation } from "@apollo/client";
+
+function LikeComponent({ user, likeDetails: { id, likes, likesCount } }) {
+  const [liked, setLiked] = useState(false);
+
+  useEffect(() => {
+    if (user && likes.find((like) => like.username === user.username)) {
+      //logged in user has liked this post
+      setLiked(true);
+    } else {
+      setLiked(false);
+    }
+  }, [user, likes]);
+
+  const [likeOnPost] = useMutation(LIKE_ON_POST, {
+    variables: { postId: id },
+  });
+
+  //   const likeOnPost = () => {
+  //     console.log("you have liked this post");
+  //   };
+
+  //done for ui changes and auth user
+  const likeButton = user ? (
+    liked ? (
+      //liked -> so icon filled
+      <Button color="red" onClick={likeOnPost}>
+        <Icon name="heart" />
+      </Button>
+    ) : (
+      //not liked it -> so icon oyutline
+      <Button color="red" basic onClick={likeOnPost}>
+        <Icon name="heart" />
+      </Button>
+    )
+  ) : (
+    <Button as="Link" to="/login" color="red" basic onClick={likeOnPost}>
+      <Icon name="heart" />
+    </Button>
+  );
+
+  return (
+    <Button as="div" labelPosition="right">
+      {likeButton}
+      <Label as="a" basic color="red" pointing="left">
+        {likesCount}
+      </Label>
+    </Button>
+  );
+}
+
+const LIKE_ON_POST = gql`
+  mutation LikePost($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      likes {
+        username
+        id
+      }
+      likesCount
+    }
+  }
+`;
+
+export default LikeComponent;
+
+
+//wonder how it is updating the cache in apollo client after mutation as we have not used any proxy
+//1. in mutation we have used id to update like for post 
+//2. after mutation happens for likPost its return type is Post 
+//3. when Post is returned the apollo gets the id and check if Post is there in cache it automatically updatees it withput writing query
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Label, Card, Image } from "semantic-ui-react";
 import moment from "moment";
+
+//get the context
+import { AuthContext } from "../context/auth";
+import LikeComponent from "./LikeComponent";
 
 function PostCard({
   post: {
@@ -15,13 +19,7 @@ function PostCard({
     createdAt,
   },
 }) {
-  const likeOnPost = () => {
-    console.log("you have liked this post");
-  };
-
-  const commentOnPost = () => {
-    console.log("you have liked this post");
-  };
+  const { user } = useContext(AuthContext);
 
   return (
     <Card fluid>
@@ -38,22 +36,27 @@ function PostCard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
+        <LikeComponent user={user} likeDetails={{ id, likes, likesCount }} />
+
         <Button as="div" labelPosition="right">
-          <Button color="red" basic onClick={likeOnPost}>
-            <Icon name="heart" />
-          </Button>
-          <Label as="a" basic color="red" pointing="left">
-            {likesCount}
-          </Label>
-        </Button>
-        <Button as="div" labelPosition="right">
-          <Button color="blue" basic onClick={commentOnPost}>
+          <Button color="blue" basic as={Link} to={`/posts/${id}`}>
             <Icon name="comments" />
           </Button>
           <Label as="a" basic color="blue" pointing="left">
             {commentCount}
           </Label>
         </Button>
+
+        {user && user.username === username && (
+          <Button
+            as="div"
+            color="red"
+            floated="right"
+            onClick={() => console.log("delete post", id)}
+          >
+            <Icon name="trash" style={{ margin: 0 }} />
+          </Button>
+        )}
       </Card.Content>
     </Card>
   );
